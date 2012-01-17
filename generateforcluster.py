7172aaf1c99b9adc7b8545d632c54e2871bdb138
Pascal Spörri <pascal.spoerri@gmail.com>
2012-01-17 14:36:35 +0100

    Rangechange...

--- generateforcluster.py
+++ generateforcluster.py
@@ -20,7 +20,7 @@ from numpy import arange
 numberOfPoints = [2**17]
 radius = [2**8]
 #speed  = map(lambda x:2**x,arange(0,5,0.1))
-speed = arange(10,200,10)
+speed = arange(400,4001,400)
 angles = [[0,10,20,30,40,50,60,70,80,90]]
 
 """
